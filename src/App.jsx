35565bd39ad71cc1b6a58ef0bd3733aafbfb5040
rutaks <rootsum.dev@gmail.com>
2020-05-18 17:09:23 +0200

    Removed navbar from visualizer

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import React, { Fragment } from "react";
 import "./App.css";
 import MergeSortingVisualizer from "./components/MergeSortingVisualizer/MergeSortingVisualizer";
-import { Navbar } from "./components/Partials/Navbar";
 
 function App() {
   console.log(process.env.REACT_APP_API_ROUTE);
   return (
     <Fragment>
-      <Navbar />
       <div className="container">
         <br />
         <br />
